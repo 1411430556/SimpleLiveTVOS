01249f6bf087b654a3e913af222c5d62bddfb845
PangChong <pangchongcyc@gmail.com>
2025-07-31 15:00:38 +0800

    优化搜索

--- SimpleLiveTVOS/Source/Search/SearchRoomView.swift
+++ SimpleLiveTVOS/Source/Search/SearchRoomView.swift
@@ -24,31 +24,72 @@ struct SearchRoomView: View {
         VStack {
             Text("请输入要搜索的主播名或平台链接/分享口令/房间号")
             HStack {
-                Picker(selection: $appModel.searchModel.searchTypeIndex) {
+                Menu {
                     ForEach(liveViewModel.searchTypeArray.indices, id: \.self) { index in
-                        // 需要有一个变量text。不然会自动帮忙加很多0
-                        let text = liveViewModel.searchTypeArray[index]
-                        Text(text)
+                        Button {
+                            appModel.searchModel.searchTypeIndex = index
+                        } label: {
+                            let text = liveViewModel.searchTypeArray[index]
+                            Text(text)
+                        }
                     }
+
                 } label: {
-                    Text("字体大小")
-                }
-            }
-            TextField("搜索", text: $appModel.searchModel.searchText)
-            .onSubmit {
-                if appModel.searchModel.searchTypeIndex == 0 {
-                    liveViewModel.roomPage = 1
-                    Task {
-                        await liveViewModel.searchRoomWithText(text: appModel.searchModel.searchText)
-                    }
-                }else {
-                    liveViewModel.roomPage = 1
-                    liveViewModel.searchRoomWithShareCode(text: appModel.searchModel.searchText)
+                    Text(liveViewModel.searchTypeArray[appModel.searchModel.searchTypeIndex])
                 }
                 
+                TextField("搜索", text: $appModel.searchModel.searchText)
+                    .onSubmit {
+                        if appModel.searchModel.searchTypeIndex == 0 {
+                            liveViewModel.roomPage = 1
+                            Task {
+                                await liveViewModel.searchRoomWithText(text: appModel.searchModel.searchText)
+                            }
+                        }else {
+                            liveViewModel.roomPage = 1
+                            liveViewModel.searchRoomWithShareCode(text: appModel.searchModel.searchText)
+                        }
+                        
+                    }
             }
             Spacer()
-            if appModel.searchModel.searchTypeIndex == 2 && liveViewModel.roomList.count == 0 {
+            if appModel.searchModel.searchTypeIndex == 0 && liveViewModel.roomList.count == 0 {
+                HStack {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("提示:")
+                            .font(.title3)
+                        Text("搜索结果可能不全，如搜索无结果请尝试”链接/分享口令/房间号“选项")
+                            .font(.headline)
+                        Spacer()
+                    }
+                    .foregroundStyle(.secondary)
+                    Spacer()
+                }
+            }else if appModel.searchModel.searchTypeIndex == 1 && liveViewModel.roomList.count == 0 {
+                HStack {
+                    VStack(alignment: .leading, spacing: 15) {
+                        Text("支持格式:")
+                            .font(.title3)
+                        Text("B站：【暖雪玩玩-哔哩哔哩直播】 https://b23.tv/jeBazlm || https://live.bilibili.com/404 || 404")
+                            .font(.callout)
+                        Text("Douyu: https://www.douyu.com/7180846 || 7180846 || https://www.douyu.com/lpl")
+                            .font(.callout)
+                        Text("Huya: 霸哥(房间号189201)正在直播\"冠军园区！大师局9999目前3170\" 分享自 @虎牙直播https://m.huya.com/189201?shareid=16890536033617446582&shareUid=13794774&source=ios&pid=1724691&liveid=7312248923907638255&platform=7&from=cpy&invite_code=HY76QLTk || https://www.huya.com/660000 || 660000 || https://www.huya.com/lpl")
+                            .font(.callout)
+                        Text("Douyin: 2- #在抖音，记录美好生活#【交个朋友直播间】正在直播，来和我一起支持Ta吧。复制下方链接，打开【抖音】，直接观看直播！ https://v.douyin.com/i8rhQQ2t/ 2@4.com 12/18 || https://live.douyin.com/168465302284 || 168465302284")
+                            .font(.callout)
+                        Text("网易CC: https://cc.163.com/364038534/ || 364038534")
+                            .font(.callout)
+                        Text("快手：https://live.kuaishou.com/u/Boy333ks1203 || Boy333ks1203")
+                            .font(.callout)
+                        Text("YY: 1雪儿7156正在YY直播【她正在唱歌】，上YY 陪你一起唱！ 丫 https://www.yy.com/share/i/v2?platform=5&config_id=55&edition=1&sharedOid=5e9b07f01bcb028e927bf7932f74bc1b&userUid=a5a06fc73370b2af419dbe7f81ca540a&sid=87208093&ssid=87208093&timestamp=1718161221&version=8.40.0 丫，复制此消息，打开【YY直播】，直接观看！| https://www.yy.com/54880976/54880976?tempId=16777217 | 54880976")
+                            .font(.callout)
+                        Spacer()
+                    }
+                    .foregroundStyle(.secondary)
+                    Spacer()
+                }
+            }else if appModel.searchModel.searchTypeIndex == 2 && liveViewModel.roomList.count == 0 {
                 HStack {
                     VStack(alignment: .leading, spacing: 10) {
                         Text("可选格式:")
